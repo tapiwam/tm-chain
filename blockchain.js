1e4b2818709dc8444d2f48c8ab042df040a2b5b8
Tapiwa <tapiwa.maruni@gmail.com>
2018-03-11 10:38:51 -0500

    Added method to validate chain is it has a bad genesis block or one of the block sis bad

--- blockchain.js
+++ blockchain.js
@@ -11,6 +11,28 @@ class Blockchain {
 
         return block;
     }
+
+    isValidChain(chain){
+
+        // Check to make sure that the incoming block has a valid genesis block
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+        // Loop through each block of the passed chain and validate
+        for(let i=1; i<chain.length; i++){
+            const block = chain[i];
+            const lastBlock = chain[i-1];
+
+            // Check that the hashes are consistent from block to block. Also check that the data inside each block is valid
+            if(block.lastHash !== lastBlock.hash ||
+                block.hash !== Block.blockHash(block)
+            ){
+                return false;
+            }
+
+            return true;
+        }
+
+    }
 }
 
 module.exports = Blockchain;
--- blockchain.test.js
+++ blockchain.test.js
@@ -3,10 +3,11 @@ const Block = require('./block');
 
 describe ('Blockchain', () => {
 
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('starts with genesis block', () => {
@@ -23,5 +24,23 @@ describe ('Blockchain', () => {
         expect(bc.chain.length).toEqual(size+1);
     });
 
+    it('validates a valid chain', () => {
+        bc2.addBlock('foo');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a valid chain with a corrupt genesis block', () => {
+
+        bc2.chain[0].data = 'Bad data';
+        bc2.addBlock('foo');
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not foo';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
 
 })
