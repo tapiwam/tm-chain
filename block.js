893fa3fa4a3a283877bbc791fb015f8d2654be5f
Tapiwa <tapiwa.maruni@gmail.com>
2018-03-11 10:00:59 -0500

    Added hashing method and the first test script for block

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
 
     constructor(timestamp, lastHash, hash, data){
@@ -23,9 +25,13 @@ class Block {
     static mineBlock(lastBlock, data){
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = 'todo-hash';
+        const hash = Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
