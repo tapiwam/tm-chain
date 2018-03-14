bb435a921b532e03ae8f796cc2c2a2b8c1501b65
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-03-13 22:17:24 -0500

    Added createTransaction method in the wallet which references the pool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -21,6 +21,10 @@ class TransactionPool {
         }
     }
 
+    existingTransaction(address){
+        return this.transactions.find(t => t.input.address === address);
+    }
+
 }
 
 module.exports = TransactionPool;
