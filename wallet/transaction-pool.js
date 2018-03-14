403e11fded0a1618bf75da17668ba2220f68323f
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-03-13 21:57:03 -0500

    Complete basic pool logic

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -8,16 +8,17 @@ class TransactionPool {
 
     updateOrAddTransaction(transaction){
 
-        if(!Transaction.verifyTransaction(transaction)){
+        /*if(!Transaction.verifyTransaction(transaction)){
             console.log("Unable to verify transaction.");
             return;
-        }
-
-        /*let existingTransaction = this.transactions.find(tran => tran.id === transaction.id );
-
-        if(existingTransaction){
-
         }*/
+
+        let transactionWithId = this.transactions.find(tran => tran.id === transaction.id );
+        if(transactionWithId){
+            this.transactions[this.transactions.indexOf(transactionWithId)] = transaction;
+        } else {
+            this.transactions.push(transaction);
+        }
     }
 
 }
