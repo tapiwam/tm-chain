a713fc06d3cf8dd4d65a4f282d5611ba887704c8
Tapiwa <tapiwa.maruni@gmail.com>
2018-03-15 07:16:47 -0500

    Started adding endpoints for transactions to the app

--- app/index.js
+++ app/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 // Default 3001 port
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -14,6 +16,12 @@ app.use(bodyParser.json());
 
 // Initialize the blockchain
 const bc = new Blockchain();
+
+// User's Wallet
+const wallet = new Wallet();
+
+// Transaction pool
+const tp = new TransactionPool();
 
 // P2P server to distribute and sync blockchain
 const p2pServer = new P2pServer(bc);
@@ -33,6 +41,10 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
 });
 
+// Endpoint to get transactions from the transaction pool
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
 
 // Open up the server in given HTTP_PORT
 app.listen(HTTP_PORT, ()=> console.log(`Listening on port ${HTTP_PORT}`));
