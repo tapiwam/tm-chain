00ca8aa5681ee9f41eb0cb4f3b031d53f56bb96c
Tapiwa <tapiwa.maruni@gmail.com>
2018-03-18 18:02:46 -0500

    Started the miner which combines transactions into blocks. Built valid transactions method that grabs valid transactions only for miner

---
+++ app/miner.js
@@ -0,0 +1,31 @@
+
+class Miner {
+    constructor(blockchain, transactionPool, wallet, p2pServer){
+        this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.p2pServer = p2pServer;
+    }
+
+    /**
+     * 1-Grab transactions
+     * 2-Create block
+     * 3-Sync chain using tp
+     * 4-Tell Tp to clear transactions
+     */
+    mine(){
+
+        const validTransactions = this.transactionPool.validTransactions();
+
+        // include a reward for the miner
+
+        // Create a block consisting of the valid transactions
+
+        // Synchronize chains in the p2p server
+
+        // Clear the transaction pool locally and broadcast to all other transaction pools
+
+    }
+}
+
+module.exports = Miner;
